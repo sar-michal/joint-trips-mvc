91f292f2f9ebc36ee096927113ed043c4da88132
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-24 23:22:51 +0100

    Change User to accommodate ASP.NET Identity

---
+++ JointTrips/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+namespace JointTrips.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Trip> OwnedTrips { get; set; } = new List<Trip>();
+        public virtual ICollection<Trip> TripsSignedUp { get; set; } = new List<Trip>();
+    }
+}
