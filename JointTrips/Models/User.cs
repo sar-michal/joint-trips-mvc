bc77b27a85815ea3db871f9643e92a9f6cee9a98
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-24 20:44:49 +0100

    Add User model

---
+++ JointTrips/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JointTrips.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public required string Username { get; set; }
+        [Required]
+        public required string PasswordHash { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public virtual ICollection<Trip> OwnedTrips { get; set; } = new List<Trip>();
+        public virtual ICollection<Trip> TripsSignedUp { get; set; } = new List<Trip>();
+    }
+}
