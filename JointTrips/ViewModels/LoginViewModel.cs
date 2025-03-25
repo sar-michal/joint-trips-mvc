007357141696990360f2d7837d41840cd123f2dd
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-25 19:31:56 +0100

    Add login and register view models

---
+++ JointTrips/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JointTrips.ViewModels
+{
+    public class LoginViewModel
+    {
+
+    [Required(ErrorMessage = "Username is required")]
+    public string? Username { get; set; }
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
+    [Display(Name = "Remember Me")]
+    public bool RememberMe { get; set; }
+    }        
+}
